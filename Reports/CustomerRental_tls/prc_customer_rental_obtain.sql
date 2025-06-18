b42d44db70396e5100b9d3d6c6fb54b59def18a1
Lasha75 <lkhvichia@yahoo.com>
2025-06-18 16:54:20 +0400

    180625

---
+++ Reports/CustomerRental_tls/prc_customer_rental_obtain.sql
@@ -0,0 +1,46 @@
+create or replace procedure "LK".prc_customer_rental_obtain()
+    language plpgsql
+as
+$$
+begin
+    truncate table "LK".lk_customer_rental restart identity;
+
+    insert into "LK".lk_customer_rental (customer_number,
+                                         customer_id,
+                                         "CUSTKEY",
+                                         "FIRST_NAME",
+                                         "LAST_NAME",
+                                         "COMMERCIAL_NAME",
+                                         "PERSON_ID",
+                                         "TAXID",
+                                         "START_DATE",
+                                         "END_DATE",
+                                         "ENTER_DATE",
+                                         "COMPANY_ID")
+    select cu.customer_number,
+           cu.id,
+           cur."CUSTKEY",
+           cur."FIRST_NAME",
+           cur."LAST_NAME",
+           cur."COMMERCIAL_NAME",
+           cur."PERSON_ID",
+           cur."TAXID",
+           cur."START_DATE",
+           cur."END_DATE",
+           cur."ENTER_DATE",
+           "COMPANY_ID"
+    from prx_customer cu
+    join "LK".lk_f_customer_rental cur on cur."CUSTKEY" = cu.cust_key
+    where cu.deleted_by is null;
+
+exception
+    when no_data_found then
+        raise notice 'Outer Exception. No Data Found % %', SQLSTATE, SQLERRM;
+        rollback;
+    when others then
+        raise notice 'Outer Exception. Others % %', SQLSTATE, SQLERRM;
+        rollback;
+end ;
+$$;
+
+alter procedure "LK".prc_customer_rental_obtain() owner to "Billing";
--- Reports/CustomerRental_tls/tbl_customer_rental.sql
+++ Reports/CustomerRental_tls/tbl_customer_rental.sql
@@ -1,18 +1,18 @@
 create table if not exists "LK".lk_customer_rental
 (
-    id              integer generated always as identity,
-    customer_number text,
-    customer_id uuid,
-        "CUSTKEY" numeric,
-        "FIRST_NAME" varchar,
-        "LAST_NAME" varchar,
-        "COMMERCIAL_NAME" varchar,
-        "PERSON_ID" varchar,
-        "TAXID" varchar,
-        "START_DATE" date,
-        "END_DATE" date,
-        "ENTER_DATE" date,
-        "COMPANY_ID" numeric
+    id                integer generated always as identity,
+    customer_number   text,
+    customer_id       uuid,
+    "CUSTKEY"         numeric,
+    "FIRST_NAME"      varchar,
+    "LAST_NAME"       varchar,
+    "COMMERCIAL_NAME" varchar,
+    "PERSON_ID"       varchar,
+    "TAXID"           varchar,
+    "START_DATE"      date,
+    "END_DATE"        date,
+    "ENTER_DATE"      date,
+    "COMPANY_ID"      numeric
 );
 
 alter table "LK".lk_customer_rental owner to "Billing";
